aa04646ab2189f87958bace068ce1b0efbcb9bbc
qitmac001060 <qitmac001060@jingjingMacBookPro.local>
2020-01-09 18:07:08 +0800

    add

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    title: 'Sunflower',  // 网站的标题
+    description: 'Vuepress blog demo',   // 网站的描述
+    themeConfig: {
+        repo: 'https://github.com/Sunflowerjing/Sunflowerjing.github.io', // GitHub仓库地址
+        repoLabel: 'GitHub', // 自定义仓库链接文字。
+        nav: [
+            { text: '博客', link: '/blog/' },
+            { text: '零散知识', link: '/scattered/'},
+            { text: '关于', link: '/about/' }
+        ],
+        sidebar: {
+            '/blog/':[
+                {
+                    title: '博客标题一',
+                    children: [
+                        'blog1-1',
+                        'blog1-2'
+                    ]
+                },
+                {
+                    title: '博客标题二',
+                    children: [
+                        'blog2-1',
+                        'blog2-2'
+                    ]
+                }
+            ],
+            '/scattered/':[
+
+            ],
+            '/about/': [
+                
+            ]
+        }
+    }
+}
