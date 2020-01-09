bf23a0dc5ef4d6f5c84e619d2e34ee6d8b33291c
qitmac001060 <qitmac001060@jingjingMacBookPro.local>
2020-01-09 14:47:41 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,14 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'http://sunflowerjing.com/' > CNAME
+echo 'http://sunflowerjing.com/' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:Sunflowerjing/Sunflowerjing.github.io.git master
+git push -f git@github.com:Sunflowerjing/Sunflowerjing.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
